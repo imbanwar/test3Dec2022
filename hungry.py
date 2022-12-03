90e18c1a5ccb3f8b54da0e2a2ad32bd6110352f0
unknown <imbesatanwar90@gmail.com>
2022-12-03 20:13:48 +0530

    files are added

--- hungry.py
+++ hungry.py
@@ -3,3 +3,9 @@ if hungry== "yes":
     print("Eat samosa")
 else:
     print("do your homework")
+
+pro = input("Are you developer")
+if pro== "dev":
+    print("Write a code")
+else:
+    print("review the code")
